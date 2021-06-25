9fd7a0016430fcf4b07ec8409ede2ba9cb19cc21
Thea Barnes <thea@tsbarnes.com>
2021-06-25 14:12:03 -0400

    Added URL field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_16_073411) do
+ActiveRecord::Schema.define(version: 2021_06_25_180716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 2021_06_16_073411) do
     t.integer "order"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "url"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
